1d1c1f7f2d17183656b9f64a4ed3ab4307f0840d
Enes Terzi <38747547+enestrz@users.noreply.github.com>
2020-10-23 23:03:45 +0300

    Added Mvc to application

--- EnesTeri.BookStore/EnesTeri.BookStore/Startup.cs
+++ EnesTeri.BookStore/EnesTeri.BookStore/Startup.cs
@@ -16,6 +16,8 @@ namespace EnesTeri.BookStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddMvc();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,11 +32,9 @@ namespace EnesTeri.BookStore
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
+
         }
     }
 }
